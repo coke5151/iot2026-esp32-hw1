59730294a7e77a3c2d2555b616a5de40b1085b73
pytree <houjunqimail@gmail.com>
2026-03-10 10:50:38 +0800

    feat: 成功讀取 DHT11 數值

--- edge/src/main.cpp
+++ edge/src/main.cpp
@@ -1,18 +1,26 @@
-#include <Arduino.h>
-
-// put function declarations here:
-int myFunction(int, int);
+#include "DHT.h"
+#define DHTPIN 13     // 設定 DATA 接在 Pin 13 (D13)
+#define DHTTYPE DHT11 // 定義感測器型號
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+  dht.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  delay(2000); // DHT11 讀取頻率較慢，建議間隔 2 秒以上
+  float h = dht.readHumidity(); // 讀取濕度
+  float t = dht.readTemperature(); // 讀取攝氏溫度
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  if (isnan(h) || isnan(t)) {
+    Serial.println("讀取失敗!");
+    return;
+  }
+  Serial.print("濕度: ");
+  Serial.print(h);
+  Serial.print(" %t\t");
+  Serial.print("溫度: ");
+  Serial.print(t);
+  Serial.println(" *C");
 }
